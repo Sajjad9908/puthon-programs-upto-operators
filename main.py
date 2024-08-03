bebe60797e70141b48b8a16504c571a361d17605
Sajjad9908 <166428462+Sajjad9908@users.noreply.github.com>
2024-08-03 14:18:10 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+a= float (14.4);
+b= float (25.4);
+print(a+b)
+personname = "sajjad";
+personage= "23"
+address= "mohalla makrani sehwan"
+print(personage)
+print(personname)
+print(address)
+c =23;
+d= 25;
+print(c)
+print(d)
+temp=c;
+c=d;
+d=temp;
+print("a after swapping",a)
+print("b after spping",b)
+
+a=23.3
+print(type(a),a)
+b=24
+print(type(b),b)
+a=int(23.3)
+print(type(a),a)
+b=float(24)
+print(type(b),b)
+
+studentname=input("enter student name")
+student_id=int(input("enter id"))
+print(studentname)
+print(student_id)
+a=int(input("enter any integer number"))
+print(a)
+print(type(a))
+a=float(a)
+print(a)
+print(type(a))
+a=23.4
+b=24.5
+print("value of a before swapping",a)
+print("valiue of b before swapping",b)
+a,b=b,a
+print(a,"a after swapping")
+print(b,"after swapping")
+
+print(type(a))
+print(type(b))
+a=int(a)
+a=4;
+b=6;
+c=7;
+print (not(a>b or b<c));
+"""assignment operator
+1 :is =
+2 :is +=
+3:-=
+4=*="""
+intialscore=6
+finalscore=6
+intialscore +=4
+finalscore -=6
+print(intialscore)
+print(finalscore)
+"""identity operator
+is
+is not
+"""
+a=1234
+b=1234
+print(a is b)
+"""Bitwise operator
+bitweise operator are used to compare binary numbers
+types
+& and
+| or
+^ xor
+"""
+a=10
+b=8
+print (a & b)
